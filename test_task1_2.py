909bb9341276d402f3b7908df638d888fc10b1bd
igor <ikozitskiu@gmail.com>
2024-08-25 23:35:11 +0300

    added class to test

--- test_task1_2.py
+++ test_task1_2.py
@@ -1,33 +1,37 @@
 import pytest
 from modules.db import Database
 
-db = Database()
 
-@pytest.mark.task1
-def test_database_connection(db_conn):
-    assert db_conn.test_connection()
+class TestBufferPage:
+    db = Database()
 
-@pytest.mark.task1
-@pytest.mark.parametrize('search_param', (db.get_random_record()))
-def test_check_admin_with_parameter(db_conn, driver_conn, search_param):
-    normalized_data_from_admin = driver_conn.search_filtered_records(search_param)
-    data_from_db = db_conn.search_with_random_parameter(search_param)
+    @pytest.mark.task1
+    def test_database_connection(self):
+        assert self.db.test_connection()
 
-    assert len(normalized_data_from_admin['product_name']) == len(data_from_db['product_name'])
+    @pytest.mark.task1
+    @pytest.mark.parametrize('search_param', (db.get_random_record()))
+    def test_check_admin_with_parameter(self, driver_conn, search_param):
+        normalized_data_from_admin = driver_conn.search_filtered_records(search_param)
+        data_from_db = self.db.search_with_random_parameter(search_param)
 
-    for param_name in normalized_data_from_admin:
-        assert normalized_data_from_admin[param_name] == data_from_db[param_name]
+        assert len(normalized_data_from_admin['product_name']) == len(data_from_db['product_name'])
 
-@pytest.mark.task2
-@pytest.mark.parametrize('parameter', [
-    'Ajax Socket',
-    'Ajax Hub',
-    'Ajax HubKit',
-    'Ajax CenterButton (2-gang) vertical [55]'
-])
-def test_socket_quantity(db_conn, driver_conn, parameter):
-    res_from_db = db_conn.search_for_product(parameter)
-    res_from_admin = driver_conn.get_quantity_for_product(parameter)
-    assert res_from_db == res_from_admin
+        for param_name in normalized_data_from_admin:
+            assert normalized_data_from_admin[param_name] == data_from_db[param_name]
 
-db.close()
+    @pytest.mark.task2
+    @pytest.mark.parametrize('parameter', [
+        'Ajax Socket',
+        'Ajax Hub',
+        'Ajax HubKit',
+        'Ajax CenterButton (2-gang) vertical [55]'
+    ])
+    def test_socket_quantity(self, driver_conn, parameter):
+        res_from_db = self.db.search_for_product(parameter)
+        res_from_admin = driver_conn.get_quantity_for_product(parameter)
+        assert res_from_db == res_from_admin
+
+    @classmethod
+    def shutdown_db(cls):
+        cls.db.close()
